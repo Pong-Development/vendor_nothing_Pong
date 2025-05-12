65cd035ca15c3552adaa80ae1edb2bcea8d00f5f
Ghosuto <clash.raja10@gmail.com>
2025-05-12 12:12:32 +0000

    Revert "Pong: Sync with DT"
    This reverts commit 6b6964a2d28c58965b79c42bbed64a7603ff877e.

--- Pong-vendor.mk
+++ Pong-vendor.mk
@@ -758,10 +758,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl-qti \
     android.hardware.gnss-aidl-impl-qti \
     android.hardware.gnss@2.1-impl-qti \
+    audio.primary.taro \
     camera.qcom \
     com.dsi.ant@1.0-impl \
     com.qti.chi.override \
     fingerprint.default \
+    sound_trigger.primary.taro \
     vendor.qti.esepowermanager@1.1-impl \
     vendor.qti.hardware.alarm@1.0-impl \
     vendor.qti.hardware.bluetooth_audio@2.0-impl \
@@ -817,15 +819,23 @@ PRODUCT_PACKAGES += \
     libTrustedUI \
     libTrustedUITZ \
     libTrustedUIVM \
+    lib_bt_aptx \
+    lib_bt_ble \
+    lib_bt_bundle \
     lib_lvacfs \
     libadm \
     libadreno_app_profiles \
     libadreno_utils \
     libadsp_default_listener \
     libadsprpc \
+    libagm \
     libaidenoiser \
     libaidenoiserv2 \
     libalCFR \
+    libar-acdb \
+    libar-gpr \
+    libar-gsl \
+    libar-pal \
     libarcsoft_aiscenedetection \
     libarcsoft_base_utils \
     libarcsoft_beautyshot \
@@ -852,6 +862,7 @@ PRODUCT_PACKAGES += \
     libarcsoft_turbo_fusion_raw_portrait_super_night \
     libarcsoft_turbo_fusion_raw_super_night \
     libarcsoft_watermark \
+    libats \
     libaudio_log_utils \
     libbacklight-calib \
     libbatching \
@@ -989,6 +1000,8 @@ PRODUCT_PACKAGES += \
     liblowi_wifihal \
     liblqe \
     liblvacfs_wrapper \
+    liblx-ar_util \
+    liblx-osal \
     libmasc \
     libmcs \
     libmctfengine_stub \
@@ -1275,6 +1288,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.gnss-V3-ndk_platform \
     vendor.qti.gnss-V5-ndk_platform \
     vendor.qti.gnss-service \
+    vendor.qti.hardware.AGMIPC@1.0-impl \
+    vendor.qti.hardware.ListenSoundModel@1.0-impl \
+    vendor.qti.hardware.ListenSoundModel@1.0 \
     vendor.qti.hardware.alarm@1.0 \
     vendor.qti.hardware.bluetooth_sar@1.0 \
     vendor.qti.hardware.bluetooth_sar@1.1 \
@@ -1311,6 +1327,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.limits@1.0 \
     vendor.qti.hardware.limits@1.1 \
     vendor.qti.hardware.mwqemadapter@1.0 \
+    vendor.qti.hardware.pal@1.0-impl \
     vendor.qti.hardware.qccsyshal@1.0_vendor \
     vendor.qti.hardware.qccsyshal@1.1_vendor \
     vendor.qti.hardware.qccvndhal@1.0-halimpl \
